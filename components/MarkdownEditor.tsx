79b8df7f18bc478ac42fba027514383c6bb6c7c6
pedrobourguignon <pedrobourguignon@hotmail.com>
2024-10-07 17:44:23 -0300

    feat: create MarkdownEditor component and PrimaryButton component

---
+++ components/MarkdownEditor.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { PrimaryButton } from "./Buttons/PrimaryButton";
+
+export const MarkDownEditor = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <PrimaryButton size="sm">Click here</PrimaryButton>
+    </div>
+  );
+};
